320ca493015e508dbc8d77d1ab7e7700fd38f677
rebecarv <saudeefood@gmail.com>
2024-09-08 19:40:27 -0300

    Converter box styles

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 
+// Convert Now button smooth scrolling
+document.getElementById('convertNow').addEventListener('click', function() {
+    document.getElementById('box').scrollIntoView({ behavior: 'smooth' });
+});
